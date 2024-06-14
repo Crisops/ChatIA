c69ac5664470f4df4e195ad679161ca0f03b004d
crialeperez1835 <crialeperez1835@gmail.com>
2024-06-14 22:46:44 +0000

    Comunication with IA sending message of user

--- src/components/InformationProgressIA.jsx
+++ src/components/InformationProgressIA.jsx
@@ -8,31 +8,55 @@ const InformationProgressIA = () => {
 
     const [progressIA, setProgressIA] = useState("");
     const [erroProgress, setErrorProgress] = useState(null);
+    const [engineMLC, setEngineMLC] = useState(null);
+    const [botMessages, setBotMessage] = useState([])
 
-    const {setLoadingPorgressIA} = useMessageStore(state => state)
+    const {messages, loadingProgressIA, setLoadingPorgressIA} = useMessageStore(state => state)
 
     const SELECT_MODEL = "gemma-2b-it-q4f16_1-MLC-1k"
     // const SELECT_MODEL = "Phi-3-mini-4k-instruct-q4f16_1-MLC"
-    
+
     useEffect(() =>{
-        loadingProgressIA();
-    }, [])
+        loadingProgressIAEngine();
+    }, [messages])
 
 
-    const loadingProgressIA = async () => {
+    const loadingProgressIAEngine = async () => {
         try {
-            await CreateMLCEngine(SELECT_MODEL, {
-                initProgressCallback: (info) => {
-                    const {progress, text} = info
-                    if(progress) setLoadingPorgressIA(progress)
-                setProgressIA(text)
-                }
-            })
+            if(!engineMLC){
+                const engine = await CreateMLCEngine(SELECT_MODEL, {
+                    initProgressCallback: (info) => {
+                        const {progress, text} = info
+                        if(progress && !loadingProgressIA) setLoadingPorgressIA(progress)
+                        setProgressIA(text) 
+                    }
+                })
+                setEngineMLC(engine)
+            }
+            if(messages.length !== 0){
+                const historyMessage = [...messages]
+                const { messageText } = historyMessage.pop()
+                console.log(messageText);
+                const reply = await engineMLC.chat.completions.create({
+                    messages: [
+                        ...botMessages,
+                        {
+                            role: "user",
+                            content: messageText
+                        }
+                    ]
+                })
+                console.log(reply)
+                const [botMessage] = reply.choices
+                const {message} = botMessage
+                setBotMessage(prev => [...prev, message])
+            }
         } catch (error) {
             const {message} = error
             setErrorProgress(message)
         }
     }
+
 
 
 
