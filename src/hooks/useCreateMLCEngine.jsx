9f1b14b4c106e617084a2fa66360b7663af4f629
crialeperez1835 <crialeperez1835@gmail.com>
2024-06-19 20:08:11 +0000

    Change variable model ia and delete/agg states

--- src/hooks/useCreateMLCEngine.jsx
+++ src/hooks/useCreateMLCEngine.jsx
@@ -7,16 +7,15 @@ import { updateMessagesChatIA } from "../helpers/updateMessagesChatIA";
 
 export const useCreateMLCEngine = () => {
 
-    const {messages, progressIA, setIsLoading, loadingProgressIA, setLoadingPorgressIA, setProgressIA, setSendMessage} = useMessageStore(state => state)
+    const {messages, progressIA, setIsLoading, loadingProgressIA, setLoadingPorgressIA, setProgressIA, setSendMessage, setErrorEngine} = useMessageStore(state => state)
 
     const [engineMLC, setEngineMLC] = useState(null)
-    const [errorEngine, setErrorEngine] = useState([{ message: "" }])
 
-    const getMLCEngine = async ({SELECT_MODEL}) => {
+    const getMLCEngine = async ({MODEL_ID_GEMMA}) => {
         try{
             const engine = await CreateWebWorkerMLCEngine(
                 new Worker(new URL('../workers/worker.js', import.meta.url), {type: "module"}),
-                SELECT_MODEL,
+                MODEL_ID_GEMMA,
                 {
                 initProgressCallback: (info) => {
                     const {progress, text} = info
@@ -29,8 +28,11 @@ export const useCreateMLCEngine = () => {
             })
             setEngineMLC(engine)
         }catch(error){
-            const { message } = error
-            setErrorEngine(errorMessage => [{...errorMessage, message}])
+            setErrorEngine({error: true, messageText: error})
+        } finally {
+            setTimeout(() => {
+                setProgressIA("")
+            }, 1000);
         }
 
     }
@@ -48,12 +50,11 @@ export const useCreateMLCEngine = () => {
             }
             setIsLoading(false)
         } catch (error) {
-            const { message } = error
-            setErrorEngine(errorMessage => [{...errorMessage, message}])
+            setErrorEngine({error: true, messageText: "Lo siento, al parecer hubo un error en la carga del mensaje. Vuelve a intentarlo más tarde."})
         }
 
     }
 
-    return {engineMLC, messages, progressIA, errorEngine, getMLCEngine, chatTimeRealIA}
+    return {engineMLC, messages, progressIA, getMLCEngine, chatTimeRealIA}
 
 }
