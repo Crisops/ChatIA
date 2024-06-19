dde2a69522631e47af87369e6127609a99d4a830
crialeperez1835 <crialeperez1835@gmail.com>
2024-06-19 20:01:51 +0000

    Add state error for manage progress ia

--- src/components/store/MessageStore.js
+++ src/components/store/MessageStore.js
@@ -3,12 +3,14 @@ import { create } from 'zustand'
 export const useMessageStore = create((set) => ({
   isLoading: true,
   messageForm: "",
-  progressIA: "",
+  progressIA: null,
   messages: [],
   loadingProgressIA: 0,
+  errorEngine: {error: false, messageText: ""},
   setIsLoading: (isLoading) => set({ isLoading }),
   setMessageForm: (messageForm) => set({ messageForm }),
   setProgressIA: (progressIA) => set({ progressIA }),
   setSendMessage: (updateFunction) => set((state) => ({ messages: updateFunction(state.messages) })),
-  setLoadingPorgressIA: (loadingProgressIA) => set({loadingProgressIA})
+  setLoadingPorgressIA: (loadingProgressIA) => set({loadingProgressIA}),
+  setErrorEngine: (errorEngine) => set({errorEngine})
 }))
